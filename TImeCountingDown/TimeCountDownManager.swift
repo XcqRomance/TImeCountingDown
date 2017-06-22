d3f4fa3550a52ce4d31842663ce8849715f179aa
XcqRomance <xcqromance@163.com>
2017-06-22 12:00:54 +0800

    将单例写法改为单行单利法，添加取消指定倒计时任务

--- TImeCountingDown/TimeCountDownManager.swift
+++ TImeCountingDown/TimeCountDownManager.swift
@@ -8,130 +8,154 @@
 
 import UIKit
 
-private let shareInstance = TimeCountDownManager()
-
-class TimeCountDownManager: NSObject {
-    // 单利
-    class var sharedInstance : TimeCountDownManager {
-        return shareInstance
-    }
-    
-    var pool: OperationQueue
-    
-    override init() {
-        pool = OperationQueue()
-        super.init()
-    }
-    
-    /**
-     *  开始倒计时，如果倒计时管理器里具有相同的key，则直接开始回调。
-     *
-     *  @param aKey         任务key，用于标示唯一性
-     *  @param timeInterval 倒计时总时间，受操作系统后台时间限制，倒计时时间规定不得大于 120 秒.
-     *  @param countingDown 倒计时时，会多次回调，提供当前秒数
-     *  @param finished     倒计时结束时调用，提供当前秒数，值恒为 0
-     */
-    func scheduledCountDownWith(_ key: String, timeInteval: TimeInterval, countingDown:TimeCountingDownTaskBlock?,finished:TimeCountingDownTaskBlock?) {
-
-        var task: TimeCountDownTask?
-        if coundownTaskExistWith(key, task: &task) {
-            task?.countingDownBlcok = countingDown
-            task?.finishedBlcok = finished
-            if countingDown != nil {
-                countingDown!((task?.leftTimeInterval)!)
-            }
-        } else {
-            task = TimeCountDownTask()
-            task?.leftTimeInterval = timeInteval
-            task?.countingDownBlcok = countingDown
-            task?.finishedBlcok = finished
-            task?.name = key
-            
-            pool.addOperation(task!)
-        }
-        
-    }
-    
-    /**
-     *  查询倒计时任务是否存在
-     *
-     *  @param akey 任务key
-     *  @param task 任务
-     *  @return YES - 存在， NO - 不存在
-     */
-    fileprivate func coundownTaskExistWith(_ key: String,task: inout TimeCountDownTask? ) -> Bool {
-        var taskExits = false
-        
-        for (_, obj)  in pool.operations.enumerated() {
-            
-            let temptask = obj as! TimeCountDownTask
-            if temptask.name == key {
-                task = temptask
-                taskExits = true
-                print("#####\(temptask.leftTimeInterval)")
-                break
-            }
-            
-        }
-        return taskExits
-    }
-    
-    func cancelAllTask() {
-        pool.cancelAllOperations()
-    }
-    
-    func suspendAllTask() {
-        pool.isSuspended = true
-    }
-}
-
 /// 计时中回调
 typealias TimeCountingDownTaskBlock = (_ timeInterval: TimeInterval) -> Void
 // 计时结束后回调
 typealias TimeFinishedBlock = (_ timeInterval: TimeInterval) -> Void
 
-class TimeCountDownTask: Operation {
-    var taskIdentifier: UIBackgroundTaskIdentifier = -1
-    var leftTimeInterval: TimeInterval = 0
-    var countingDownBlcok: TimeCountingDownTaskBlock?
-    var finishedBlcok: TimeFinishedBlock?
+class TimeCountDownManager: NSObject {
+  // 单行单利
+  static let sharedInstance = TimeCountDownManager()
+  
+  var pool: OperationQueue
+  
+  override init() {
+    pool = OperationQueue()
+    super.init()
+  }
+  
+  /**
+   *  开始倒计时，如果倒计时管理器里具有相同的key，则直接开始回调。
+   *
+   *  @param aKey         任务key，用于标示唯一性
+   *  @param timeInterval 倒计时总时间，受操作系统后台时间限制，倒计时时间规定不得大于 120 秒.
+   *  @param countingDown 倒计时时，会多次回调，提供当前秒数
+   *  @param finished     倒计时结束时调用，提供当前秒数，值恒为 0
+   */
+  func scheduledCountDownWith(_ key: String, timeInteval: TimeInterval, countingDown:TimeCountingDownTaskBlock?,finished:TimeCountingDownTaskBlock?) {
     
-    
-    
-    override func main() {
-        
-        if self.isCancelled {
-            return
-        }
-        taskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
-        
-        while leftTimeInterval > 0 {
-            print("----\(leftTimeInterval)")
-            print(self.isExecuting)
-            if self.isCancelled {
-                return
-            }
-            leftTimeInterval -= 1
-            DispatchQueue.main.async(execute: {
-                if self.countingDownBlcok != nil {
-                    self.countingDownBlcok!(self.leftTimeInterval)
-                }
-            })
-            Thread.sleep(forTimeInterval: 1)
-        }
-        
-        DispatchQueue.main.async {
-            if self.isCancelled {
-                return
-            }
-            if self.finishedBlcok != nil {
-                self.finishedBlcok!(0)
-            }
-        }
-        
-        if taskIdentifier != UIBackgroundTaskInvalid {
-            UIApplication.shared.endBackgroundTask(taskIdentifier)
-            taskIdentifier = UIBackgroundTaskInvalid
-        }
+    var task: TimeCountDownTask?
+    if coundownTaskExistWith(key, task: &task) {
+      task?.countingDownBlcok = countingDown
+      task?.finishedBlcok = finished
+      if countingDown != nil {
+        countingDown!((task?.leftTimeInterval)!)
+      }
+    } else {
+      task = TimeCountDownTask()
+      task?.leftTimeInterval = timeInteval
+      task?.countingDownBlcok = countingDown
+      task?.finishedBlcok = finished
+      task?.name = key
+      
+      pool.addOperation(task!)
     }
+    
+  }
+  
+  /**
+   *  查询倒计时任务是否存在
+   *
+   *  @param akey 任务key
+   *  @param task 任务
+   *  @return YES - 存在， NO - 不存在
+   */
+  fileprivate func coundownTaskExistWith(_ key: String,task: inout TimeCountDownTask? ) -> Bool {
+    var taskExits = false
+    
+    for (_, obj)  in pool.operations.enumerated() {
+      
+      let temptask = obj as! TimeCountDownTask
+      if temptask.name == key {
+        task = temptask
+        taskExits = true
+        printLog("#####\(temptask.leftTimeInterval)")
+        break
+      }
+      
+    }
+    return taskExits
+  }
+  
+  /**
+   *  取消所有倒计时任务
+   */
+  func cancelAllTask() {
+    pool.cancelAllOperations()
+  }
+  
+  /**
+   *  挂起所有倒计时任务
+   */
+  func suspendAllTask() {
+    pool.isSuspended = true
+  }
+  
+  /**
+   *  取消指定任务的倒计时任务
+   */
+  func cancelTask(_ name: String) {
+    for operation in pool.operations {
+      let task = operation as! TimeCountDownTask
+      let taskName = task.name ?? ""
+      if taskName == name {
+        task.cancel()
+      }
+    }
+  }
+}
+
+class TimeCountDownTask: Operation {
+  
+  var taskIdentifier: UIBackgroundTaskIdentifier = -1
+  var leftTimeInterval: TimeInterval = 0
+  var countingDownBlcok: TimeCountingDownTaskBlock?
+  var finishedBlcok: TimeFinishedBlock?
+  
+  override func main() {
+    
+    if self.isCancelled {
+      return
+    }
+    taskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
+    
+    while leftTimeInterval > 0 {
+      printLog("----\(leftTimeInterval)")
+      printLog(self.isExecuting)
+      if self.isCancelled {
+        return
+      }
+      leftTimeInterval -= 1
+      DispatchQueue.main.async(execute: {
+        if self.countingDownBlcok != nil {
+          self.countingDownBlcok!(self.leftTimeInterval)
+        }
+      })
+      Thread.sleep(forTimeInterval: 1)
+    }
+    
+    DispatchQueue.main.async {
+      if self.isCancelled {
+        return
+      }
+      if self.finishedBlcok != nil {
+        self.finishedBlcok!(0)
+      }
+    }
+    
+    if taskIdentifier != UIBackgroundTaskInvalid {
+      UIApplication.shared.endBackgroundTask(taskIdentifier)
+      taskIdentifier = UIBackgroundTaskInvalid
+    }
+  }
+}
+
+public func printLog<T>(_ message: T,
+                     file: String = #file,
+                     method: String = #function,
+                     line: Int = #line)
+{
+  #if DEBUG
+    print("\((file as NSString).lastPathComponent)[\(line)], \(method): \(message)")
+  #endif
 }
